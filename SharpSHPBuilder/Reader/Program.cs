dc77b48272385e5ae7ef4d366b6ab6e9876e7b5e
Taryn <phrohdoh@gmail.com>
2014-06-12 22:00:24 -0500

    Thirdparty library cleanup. Moved shp -> png logic to libshp.
    Selecting files and converting via GUI is possible!
    
    Added MacApp project for eventual .app bundle.
    
    Annoying hack that forces the OpenFileDialog
    	to begin navigation from Documents directory.

--- SharpSHPBuilder/Reader/Program.cs
+++ SharpSHPBuilder/Reader/Program.cs
@@ -11,9 +11,9 @@ namespace Reader
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		public static void Main(string[] input)
 		{
-			if (args.Length < 3)
+			if (input.Length < 3)
 			{
 				Console.WriteLine("Usage:\n");
 				Console.WriteLine("Provide a source shp file and a palette (.pal).");
@@ -21,73 +21,10 @@ namespace Reader
 				return;
 			}
 
+			var args = input.Skip(1).ToArray();
+
 			if (args[0] == "--png")
-				Converter.ConvertSpriteToPng(args.Skip(1).ToArray());
-		}
-	}
-
-	static class Converter
-	{
-		public static void ConvertSpriteToPng(string[] args)
-		{
-			var src = args[0];
-			var shadowIndex = new int[] { };
-
-			var palette = Palette.Load(args[1], shadowIndex);
-
-			ISpriteSource source;
-
-			using (var stream = File.OpenRead(src))
-				if (stream == null)
-					return;
-
-			using (var stream = File.OpenRead(src))
-				source = SpriteSource.LoadSpriteSource(stream, src);
-
-			// The r8 padding requires external information that we can't access here.
-			var usePadding = false; // !(args.Contains("--nopadding") || source is R8Reader);
-			var count = 0;
-			var prefix = Path.GetFileNameWithoutExtension(src);
-
-			foreach (var frame in source.Frames)
-			{
-				var frameSize = usePadding ? frame.FrameSize : frame.Size;
-				var offset = usePadding ? (frame.Offset - 0.5f * new float2(frame.Size - frame.FrameSize)).ToInt2() : int2.Zero;
-
-				// shp(ts) may define empty frames
-				if (frameSize.Width == 0 && frameSize.Height == 0)
-				{
-					count++;
-					continue;
-				}
-
-				using (var bitmap = new Bitmap(frameSize.Width, frameSize.Height, PixelFormat.Format8bppIndexed))
-				{
-					bitmap.Palette = palette.AsSystemPalette();
-					var data = bitmap.LockBits(new Rectangle(0, 0, frameSize.Width, frameSize.Height),
-						ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
-
-					// Clear the frame
-					if (usePadding)
-					{
-						var clearRow = new byte[data.Stride];
-						for (var i = 0; i < frameSize.Height; i++)
-							Marshal.Copy(clearRow, 0, new IntPtr(data.Scan0.ToInt64() + i * data.Stride), data.Stride);
-					}
-
-					for (var i = 0; i < frame.Size.Height; i++)
-					{
-						var destIndex = new IntPtr(data.Scan0.ToInt64() + (i + offset.Y) * data.Stride + offset.X);
-						Marshal.Copy(frame.Data, i * frame.Size.Width, destIndex, frame.Size.Width);
-					}
-
-					bitmap.UnlockBits(data);
-
-					var filename = "{0}-{1:D4}.png".F(prefix, count++);
-					bitmap.Save(filename);
-				}
-			}
-			Console.WriteLine("Saved {0}-[0..{1}].png", prefix, count - 1);
+				Commands.ConvertSpriteToPng(args);
 		}
 	}
 }
--- SharpSHPBuilder/SharpSHPBuilder/MainWindow.cs
+++ SharpSHPBuilder/SharpSHPBuilder/MainWindow.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Eto;
 using Eto.Forms;
 using Eto.Drawing;
+using libshp;
 
 namespace SharpSHPBuilder
 {
 	public class MainWindow : Form
 	{
+		static Platform platform;
+
 		public MainWindow()
 		{
 			Title = "EtoForm";
@@ -14,21 +19,71 @@ namespace SharpSHPBuilder
 
 			var layout = new DynamicLayout();
 
-			var tb = new TextBox();
-			var input = new OpenFileDialog();
+			var shpFilenameLabel = new Label();
+			var palFilenameLabel = new Label();
+			var operationLabel = new Label() { Text = "No operation." };
 
-			var testbtn = ButtonExts.EventButton("open file", (sender, e) =>
+			var openDialog = new OpenFileDialog();
+
+			openDialog.CheckFileExists = true;
+
+			var selectShpFileButton = ButtonExts.EventButton("Select SHP", (sender, e) =>
 				{
-					input.ShowDialog(layout);
+					// nasty HACK as workaround to "Recent Items" dir causing NRE
+					openDialog.Directory = new Uri(EtoEnvironment.GetFolderPath(EtoSpecialFolder.Documents));
+
+					openDialog.ShowDialog(shpFilenameLabel);
+					shpFilenameLabel.Text = openDialog.FileName ?? "No shp!";
+					operationLabel.Text = "Selected shp file.";
 				});
 
-			var exitButton = ButtonExts.EventButton("Quit!", (sender, e) => { Environment.Exit(-1); });
+			var selectPalFileButton = ButtonExts.EventButton("Select PAL", (sender, e) =>
+				{
+					// nasty HACK as workaround to "Recent Items" dir causing NRE
+					openDialog.Directory = new Uri(EtoEnvironment.GetFolderPath(EtoSpecialFolder.Documents));
 
-			tb.Text = input.CheckFileExists ? input.FileName : "No filename!";
+					openDialog.ShowDialog(palFilenameLabel);
+					palFilenameLabel.Text = openDialog.FileName ?? "No pal!";
+					operationLabel.Text = "Selected pal file.";
+				});
 
-			layout.BeginHorizontal();
-			layout.AddColumn(testbtn, exitButton, tb);
-			layout.EndHorizontal();
+			var convertToPngButton = ButtonExts.EventButton("Convert to png.", (sender, e) =>
+				{
+					var shp = shpFilenameLabel.Text;
+					var pal = palFilenameLabel.Text;
+
+					if (string.IsNullOrEmpty(shp) || string.IsNullOrEmpty(pal))
+					{
+						Console.WriteLine("The shp or pal file is null or was not selected.");
+						return;
+					}
+
+					if (!shpFilenameLabel.Text.Contains(".shp") || !palFilenameLabel.Text.Contains(".pal"))
+					{
+						Console.WriteLine("One of the selected files is not the correct file type (extension).");
+						return;
+					}
+
+					Commands.ConvertSpriteToPng(new[] {shp, pal} );
+					operationLabel.Text = "Converted {0} to png!".F(shp);
+				});
+
+			var quitButton = ButtonExts.EventButton("Quit!", (sender, e) => { Environment.Exit(-1); });
+
+//			// OSX menubar
+//			if (platform.IsMac) // if (Generator.Supports<MenuBar>())
+//			{
+//				var menuBar = new MenuBar();
+//				this.Menu = menuBar;
+//			}
+
+			layout.AddColumn
+			(
+				selectShpFileButton, selectPalFileButton,
+				convertToPngButton, quitButton,
+				shpFilenameLabel, palFilenameLabel,
+				operationLabel
+			);
 
 			Content = layout;
 		}
@@ -36,11 +91,10 @@ namespace SharpSHPBuilder
 		[STAThread]
 		static void Main()
 		{
-			var generator = Generator.GetGenerator(Generators.GtkAssembly);
-			var app = new Application(generator);
+			// OSX is *supposed to be* handled by our MacApp project, but isn't yet
+			platform = Platform.Get(EtoEnvironment.Platform.IsWindows ? Platforms.WinForms : Platforms.Gtk2);
 
-			var button = new Button();
-
+			var app = new Application(platform);
 			app.Initialized += delegate
 			{
 				app.MainForm = new MainWindow();
